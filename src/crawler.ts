b783f28fc149eed1e3ca3e6a0ca2b8e088d8fdd8
Pedro Worcel <pedro@worcel.com>
2021-06-22 19:15:45 -0700

    Implement basic pupetteer PoC

---
+++ src/crawler.ts
@@ -0,0 +1,19 @@
+//import { getLogger } from "./logger";
+import puppeteer from 'puppeteer'
+
+//let logger = getLogger("poller")
+
+const url = process.argv[2];
+if (!url) {
+    throw "Please provide a URL as the first argument";
+}
+
+async function run () {
+    const browser = await puppeteer.launch();
+    const page = await browser.newPage();
+    await page.goto(url);
+    await page.screenshot({path: 'screenshot.png'});
+    browser.close();
+}
+run();
+
