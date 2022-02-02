728e11b0937eef99941652c6ab39bb7a8408fc31
Sam Joan <sam@roque-worcel.com>
2022-02-02 14:13:04 +1300

    Fix proxy server URL.

--- src/poller.ts
+++ src/poller.ts
@@ -51,7 +51,7 @@ amqp.connect(connCreds, function(error0, connection) {
         //tcp6       0      0 :::8085                 :::*                    LISTEN      2162986/python
         //tcp6       0      0 :::8086                 :::*                    LISTEN      2163517/python
         let port:number = 8000 + randomInt(1, 6);
-        let proxy = 'unicornbottle-main:' + port;
+        let proxy = 'http://unicornbottle-main:' + port;
         logger.info(`Using proxy ${proxy}`);
         const browser = await chromium.launch({
             proxy: {
